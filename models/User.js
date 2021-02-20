f8751a8c67daadda89f91a94b717d47c587dbff6
Hani Alfiyyah <hanii.alfiyyah@gmail.com>
2021-02-20 10:52:02 +0700

    add login and register

--- models/User.js
+++ models/User.js
@@ -1,7 +1,32 @@
-const { getDb } = require('../config/mongodb');
+const { ObjectID } = require('mongodb')
+const { getDb } = require('../config/mongodb')
+const USER_COLLECTION = process.env.USER_COLLECTION
 
 class User {
+  static database() {
+    return getDb().collection(USER_COLLECTION)
+  }
+  static register(data) {
+    return this.database().insertOne(data)
+  }
+  static findAll() {
+    return this.database().find().toArray()
+  }
+  static findById(id) {
+    return this.database().findOne({ _id: ObjectID(id) })
+  }
+  static findByEmail(email) {
+    return this.database().findOne({ email })
+  }
+  static update(id, data) {
+    return this.database().findOneAndUpdate(
+      { _id: ObjectID(id) },
+      { $set: data }
+    )
+  }
+  static delete(id) {
+    return this.database().deleteOne({ _id: ObjectID(id) })
+  }
+}
 
-};
-
-module.exports = User;
+module.exports = User
